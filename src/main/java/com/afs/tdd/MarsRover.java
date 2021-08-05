7a4d237156838efd637bf6e11e18cdf5a8e33b7c
deverca <deverca@oocl.com>
2021-08-05 17:35:33 +0800

    [MarsRover][Carmela]test: created a should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_direction_N_and_M test case

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,17 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    private RoverStatus roverStatus;
+
+    public MarsRover(RoverStatus roverStatus) {
+        this.roverStatus = roverStatus;
+    }
+
+    public void executeCommand(String command){
+    }
+
+    public RoverStatus getRoverStatus() {
+        return null;
+    }
+}
